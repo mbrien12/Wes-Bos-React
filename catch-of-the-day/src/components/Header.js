59447e0107afc096b1af05b9eb303791d9d065d4
Michelle Brien <michelle.brien12@gmail.com>
2018-04-01 20:46:55 +0100

    routes added

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>Fresh Daily</span>
-        </h3>
-      </header>
-    );
-  }
-}
+// If only has a render method should instead be a STATELESS FUNCTIONAL COMPONENT
+
+const Header = (props) => (
+  <header className="top">
+  <h1>
+    Catch
+    <span className="ofThe">
+      <span className="of">of</span>
+      <span className="the">the</span>
+    </span>
+    Day
+  </h1>
+  <h3 className="tagline">
+    <span>{ props.tagline}</span>
+  </h3>
+</header>
+)
 
 export default Header;
  
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -3,7 +3,7 @@ import React from "react";
 class Inventory extends React.Component {
   render() {
     return (
-
+      <div className="inventory">Inventory!!</div>
     );
   }
 }
